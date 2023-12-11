f484b2f4fb1b688322c45e71ccab4867df87d2a3
JoaoVitrDeMeloGomes <joaovitordemelogomes@gmail.com>
2023-12-11 08:26:55 -0300

    meu primeiro commit

--- app.js
+++ app.js
@@ -1,39 +1,25 @@
-alert ("Boas vindas ao jogo do número secreto:");
-let numeroSecreto = 5;
-console.log (numeroSecreto)
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
-// qquando o chute não for igual ao numeroSecreto
+// enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-     chute = prompt("Escolha um número de 1 a 10 ");
-}
-// Se o chute for certo:
+    chute = prompt('Escolha um número entre 1 e 100');
+    // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-   alert( `Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
-        if (numeroSecreto > chute)
-    alert (`O número secreto é menor que ${chute}`);
-         else { 
-    alert (`O número é maior que ${chute}`);
-         }
-         tentativas++;
-
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
 
-//Dentro dos () -> bota-se o que quer comparar
-// Dentro do {} -> bota-se aquilo que caso for comparado de certo
-
-// Não precisa usar aspas para usar 
-
-// ! quando o projeto ficar disponível para todas as pessoas, o que chamamos no contexto de software de ambiente de produção, os comandos console.log devem ser removidos ou desabilitados, pois podem causar problemas de desempenho e segurança.
-// Para insirir texto no js usa-se aspas 
-/* Ás aspas podem ser duplas ou não, 
- mas se começou com "" terminar com a mesma*/
-// let é uma variavel para armazenar algo na memória
-// O nome da variavel tem que ser clor e literal com a informação dada
-// quando é número não precisa de aspas
-// Em fases de testes testamos tudo que é possivel
-// Coamndo pra dizer que um valor é igual a outro valor qque é o if 
-// if = condição 
-// !
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
